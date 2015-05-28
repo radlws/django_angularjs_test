f53d8084023a0095429ff6766d2ca0affecc2fdc
radlws <radzhome@gmail.com>
2015-05-27 21:11:52 -0400

    ratings app wip

--- static/js/ratings_app.js
+++ static/js/ratings_app.js
@@ -1,11 +1,17 @@
 
-var myApp = angular.module('ratingsApp', []);
-myApp.config(function($interpolateProvider) {
+var ratingsApp = angular.module('ratingsApp', []);
+ratingsApp.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 });
 
-//customInterpolationApp.controller('DemoController', function() {
-//    this.label = "This binding is brought you by // interpolation symbols.";
-//});
-
+ratingsApp.controller("RatingsController", function($scope) {
+    $scope.ratings=["hi@email.com","hello@email.com", "radzhome@gmail.com"];
+    //$scope.ratings=["Rad: 1", ];
+    //$scope.ratings=[];
+    $scope.add=function(){
+        $scope.ratings.push($scope.new_rating);
+        $scope.new_rating="";
+        console.log($scope.ratings)
+    }
+});
