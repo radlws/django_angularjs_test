d76327c5c07c75a3a4711504b973ab5812a33eac
Rad W <radzhome@gmail.com>
2015-06-04 22:52:12 -0400

    cleanup

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import patterns, url
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 urlpatterns = patterns('',
     url(r'home/$', 'events.views.events_home', name='events_home'),
+
+    url(r'^$',
+        lambda x: HttpResponseRedirect(reverse('events_home')),
+        name='events_home_redirect'),
 
 )
